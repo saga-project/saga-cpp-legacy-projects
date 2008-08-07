336bf5c0600f263da2e8ad998397dad66764f321
aluckow <aluckow@defb5e50-622e-49ec-a68e-d72c7db87b45>
2008-08-07 22:13:52 +0000

    Successful CPR job submission with Python
    git-svn-id: file://localhost/tmp/svn2git/svn@116 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/migol/examples/cpr/trunk/master-python/submit.py
+++ applications/migol/examples/cpr/trunk/master-python/submit.py
@@ -4,6 +4,11 @@ def submit_job():
         js = saga.cpr.service(saga.url("migol://qb1.loni.org/jobmanager-pbs"))
         jd = saga.cpr.description()
         jd.executable="/bin/date"
+	jd.workingdirectory="/tmp"
+	jd.arguments=["no", "ha"]
+	jd.numberofprocesses="1"
+	jd.output="stdout"
+	jd.error="stderr"
         namd_job = js.create_job(jd,jd)
         namd_job.run()
 
