0191e12feb039cbc2b6dbf19639b2834d224a47f
aluckow <aluckow@defb5e50-622e-49ec-a68e-d72c7db87b45>
2008-08-09 19:37:19 +0000

    Updated performance measurements in section 6 I hope I resolved all conflicts correctly...
    Andre
    
    
    git-svn-id: file://localhost/tmp/svn2git/svn@139 defb5e50-622e-49ec-a68e-d72c7db87b45

--- papers/migol-escience08/performance/gnuplot.sh
+++ papers/migol-escience08/performance/gnuplot.sh
@@ -7,7 +7,7 @@ set encoding iso_8859_1
 
 set output "perf.eps"
 #set title "Submission Times"
-set ylabel "Runtime (in s)" font "Helvetica, 20"
+set ylabel "Runtime (in s)" #font "Helvetica, 20"
 set xlabel ""
 #set xtics rotate 1 
 #set size 0.9,1.03
@@ -33,7 +33,7 @@ set nokey
 #set format x "%d.%m."
 set format y "%.0f"
 set xrange [0.5:5.7] 
-set yrange [0:50] 
+set yrange [0:30] 
 set boxwidth 0.8 relative
 set style fill solid 0.9
 # set linestyle 1 lt 1 lw 50
@@ -44,9 +44,33 @@ plot "data.txt" using 1:2:3  title "Submission Time" with boxes linetype 1  fs s
     #"data.txt" using 1:4:6 title "Resource Creation" with boxes linetype 1  fs solid 0.5
     # "data.txt" using 1:5:6 title "Delegation on Demand" with boxes fs pattern
 
-set output "perf_runtime.eps" 
-set xtics ("NAMD runtime\n (w/o SAGA/Migol) "  1, "NAMD runtime\n (with SAGA/Migol)" 2)  
+# set output "perf_runtime.eps" 
+# set xtics ("NAMD runtime\n (w/o SAGA/Migol) "  1, "NAMD runtime\n (with SAGA/Migol)" 2)  
+# set yrange [0:30]
+# set xrange [0.5:2.5]   
+# set ylabel "Runtimes (in min)" font "Helvetica, 24" 
+# plot "data2.txt" using 1:2:3  title "Submission Time" with boxes linetype 1  fs solid 0.5
+         
+      
+set output "perf_monitoring.eps"       
+unset xtics 
+set xtics (20,40,60,80,100,120,140,160,180,200,220,240)
+#set logscale x 
+set xrange [20:240]       
 set yrange [0:30]
-set xrange [0.5:2.5]   
-set ylabel "Runtimes (in min)" font "Helvetica, 24" 
-plot "data2.txt" using 1:2:3  title "Submission Time" with boxes linetype 1  fs solid 0.5
+#set logscale x 2
+set ylabel "Runtime (in min)" #font "Helvetica, 20" 
+set xlabel "Monitoring Intervall (in s)" 
+plot "data-monitoring.txt" using 1:2  with lp                     
+     
+set output "perf_remd.eps"       
+#set logscale x 2
+#set logscale x
+set xtics autofreq
+set yrange [0:50] 
+set xrange [2:16]        
+set xtics (2,4,8,16)
+set logscale x
+set ylabel "Runtime (in min)" #font "Helvetica, 20"
+set xlabel "Number Replica Processes" 
+plot "data-remd.txt" using 1:2  with lp
