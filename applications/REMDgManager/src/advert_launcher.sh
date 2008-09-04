25663ed67213b55537f9e49f8e4c0ac265d98798
aluckow <aluckow@defb5e50-622e-49ec-a68e-d72c7db87b45>
2008-09-04 20:56:15 +0000

    commented ranger specific stuff out
    git-svn-id: file://localhost/tmp/svn2git/svn@310 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/REMDgManager/src/advert_launcher.sh
+++ applications/REMDgManager/src/advert_launcher.sh
@@ -2,12 +2,12 @@
 
 . ~/.bashrc
 
-echo "load modules"
-module unload mvapich2
-module unload mvapich
-module swap pgi intel
-module load mvapich
-module load python/2.5.2
+#echo "load modules"
+#module unload mvapich2
+#module unload mvapich
+#module swap pgi intel
+#module load mvapich
+#module load python/2.5.2
 
 echo "run advert launcher"
 python `dirname $0`/advert_launcher.py $*
