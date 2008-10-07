16f5c0200577fd97d6f07bdc54622a293440d491
oweidner <oweidner@defb5e50-622e-49ec-a68e-d72c7db87b45>
2008-10-07 21:30:05 +0000

    new XCode project.
    git-svn-id: file://localhost/tmp/svn2git/svn@528 defb5e50-622e-49ec-a68e-d72c7db87b45

---
+++ applications/GridNPB-SAGA/source/main.cpp
@@ -0,0 +1,23 @@
+/*
+ *  main.cpp
+ *  GridNPB-SAGA
+ *
+ *  Created by Ole Weidner on 10/7/08.
+ *  Copyright 2008 Center for Computation & Technology. All rights reserved.
+ *
+ */
+
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <iostream>
+
+#include <boost/lexical_cast.hpp>
+#include <boost/program_options.hpp>
+
+#include <saga/saga.hpp>
+
+int main (int argc, char * const argv[]) 
+{
+    return EXIT_SUCCESS;
+}
