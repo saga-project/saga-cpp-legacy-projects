8e8b66707a0aab5645dcf1070e916bf072279128
oweidner <oweidner@defb5e50-622e-49ec-a68e-d72c7db87b45>
2008-12-01 22:02:15 +0000

    got rid of weird XHTML error...
    git-svn-id: file://localhost/tmp/svn2git/svn@651 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/FAUST/docs/apidoc/mainpage.h
+++ applications/FAUST/docs/apidoc/mainpage.h
@@ -1,16 +1,25 @@
 
-/*! \mainpage FAUST - A Framework for Adaptive Ubiquitous Scalable Tasks  
-*
-* <p align="center"><img  src="./murnau_faust.jpg" /></p>
-*
-* <hr>
-* @section notes Release Notes
-* There hasn't been a FAUST release yet. Stay tuned. 
-* 
-* <hr>
-* \section requirements Requirements
-* SAGA C++ Libraries <a href="http://saga.cct.lsu.edu">http://saga.cct.lsu.edu</a><br>
-* Boost C++ Libraries <a href="http://www.boost.org">http://www.boost.org</a>
-* <hr> 
-*
-*/
+/**
+  * @mainpage FAUST - A Framework for Adaptive Ubiquitous Scalable Tasks  
+  * 
+  * <p align="center"><img  src="./murnau_faust.jpg"></p>
+  *
+  * <hr>
+  * @section introduction Introduction
+  * Dispatching many simple jobs to execute many model simulations has a severe 
+  * limitation: all simple jobs must queue at the resource management system 
+  * potentially causing a slowdown of the overall application. We overcome this 
+  * slowdown using an efﬁcient and adaptive job dispatching schema, which allows 
+  * the clustering of model simulations processes before submission.
+  *
+  * <hr>
+  * @section notes Release Notes
+  * There hasn't been a FAUST release yet. Stay tuned. 
+  * 
+  * <hr>
+  * @section requirements Requirements
+  * SAGA C++ Libraries <a href="http://saga.cct.lsu.edu">http://saga.cct.lsu.edu</a><br>
+  * Boost C++ Libraries <a href="http://www.boost.org">http://www.boost.org</a>
+  * <hr> 
+  *
+  */
