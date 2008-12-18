3c8977fdf8c571856a84150d5fa87503d82f85a8
aluckow <aluckow@defb5e50-622e-49ec-a68e-d72c7db87b45>
2008-12-18 19:44:45 +0000

    Added reference \citep{Lei:2007xe} Added new figure with 2 y-axises from JK's data
    git-svn-id: file://localhost/tmp/svn2git/svn@716 defb5e50-622e-49ec-a68e-d72c7db87b45

---
+++ papers/migol-philtrans08/performance/hepatitis/gnuplot.sh
@@ -0,0 +1,35 @@
+set term postscript eps enhanced color linewidth 1.42 "Helvetica" 17 
+
+set encoding iso_8859_1
+
+set output "perf.eps"
+#set title "Submission Times"
+set ylabel "Time per Generation (in min)" #font "Helvetica, 20"
+set y2label "Number of Active Glide-Ins" #font "Helvetica, 20"
+set xlabel "Time (in min)"
+
+
+#set size 0.8,0.8
+#set lmargin 9
+#set bmargin 3
+#set rmargin 2
+#set tmargin 1
+                 
+#set xdata time
+#set timefmt "%d.%m%y"
+#set format x "%d.%m."
+#set format y "%.0f"
+set xrange [25:280] 
+#set y2range [0:40] 
+set yrange [0:35] 
+
+set y2range [0:9] 
+set y2tics 0, 1
+set ytics nomirror
+#set arrow .
+#set linestyle 1 lt 1 lw 50
+#"data.txt" using 1:2:6 title "Job Startup Time" with boxes linetype 1  fs solid 0.25,\
+   
+set output "perf_repex.eps" 
+plot "data.txt" using 1:2 axis x1y1 title "Time per Generation" with lp,\
+     "data.txt" using 1:3 axis x1y2 title "Number Active Glide-Ins" with lp
