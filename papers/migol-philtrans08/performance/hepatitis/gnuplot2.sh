cdf433db276651f055ed0aedc54729c64688e5e9
sjha <sjha@defb5e50-622e-49ec-a68e-d72c7db87b45>
2008-12-18 21:06:27 +0000

    changed the order of the figures 6 <-> 7
    git-svn-id: file://localhost/tmp/svn2git/svn@720 defb5e50-622e-49ec-a68e-d72c7db87b45

--- papers/migol-philtrans08/performance/hepatitis/gnuplot2.sh
+++ papers/migol-philtrans08/performance/hepatitis/gnuplot2.sh
@@ -2,9 +2,9 @@ set term postscript eps enhanced color linewidth 1.42 "Helvetica" 17
 
 set encoding iso_8859_1
 
-set ylabel "Time per Generation (in min)" #font "Helvetica, 20"
-set xlabel "Number of Active Glide-Ins" #font "Helvetica, 20"
-set y2label "Speed Up"
+set ylabel "Time per Generation (in min)"  #font "Helvetica, 20"
+set xlabel "Number of Active Glide-Ins"    #font "Helvetica, 20"
+set y2label "Speed Up" 
 
 
 #set size 0.8,0.8
@@ -27,8 +27,10 @@ set ytics nomirror
 #set linestyle 1 lt 1 lw 50
 #"data.txt" using 1:2:6 title "Job Startup Time" with boxes linetype 1  fs solid 0.25,\
    
+
 set output "perf_repex2.eps" 
 plot "data2.txt" using 1:($$3/60) axis x1y1 title "Time per Generation" with lp,\
      "data2.txt" using 1:6 axis x1y2 title "Speed Up" with lp
 
-
+#set terminal x11
+#replot
