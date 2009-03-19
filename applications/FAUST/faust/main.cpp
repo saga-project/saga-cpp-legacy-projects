ad7f4e12b5505b805631cb8ef30dc53596a69ec7
oweidner <oweidner@defb5e50-622e-49ec-a68e-d72c7db87b45>
2009-03-19 18:29:02 +0000

    fixed job submission
    git-svn-id: file://localhost/tmp/svn2git/svn@1098 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/FAUST/faust/main.cpp
+++ applications/FAUST/faust/main.cpp
@@ -20,40 +20,36 @@ int main (int argc, char* argv[])
   
   std::vector<std::string> dir_ids, dir_path, dir_dev_space_total_cmd, env;
   
-  faust::resource_description queenbee_rd;
+  faust::resource_description queenbee_rd, localhost_rd;
   
   // A directory description
-  dir_ids.push_back("my_work_dir");
-  dir_path.push_back("/home/");
-  dir_dev_space_total_cmd.push_back("df . | awk '/\// {print $2}'");
+  //dir_ids.push_back("my_work_dir");
+  //dir_path.push_back("/home/");
+  //dir_dev_space_total_cmd.push_back("df . | awk '/\// {print $2}'");
   
   // Another directory description
-  dir_ids.push_back("my_home_dir");
-  dir_path.push_back("/work/");
-  dir_dev_space_total_cmd.push_back("df . | awk '/\// {print $2}'");
+  //dir_ids.push_back("my_home_dir");
+  //dir_path.push_back("/work/");
+  //dir_dev_space_total_cmd.push_back("df . | awk '/\// {print $2}'");
   
   queenbee_rd.set_attribute("identifier", "queenbee.loni.org");
+  queenbee_rd.set_attribute("faust_agent_submit_url",  "gram://qb1.loni.org/jobmanager-fork");
+  queenbee_rd.set_attribute("faust_agent_binary_path", "/work/oweidner/FAUST/agent/faust_agent");	
+  queenbee_rd.set_attribute("saga_root_path",          "/work/oweidner/megajobs");
   
-  //queenbee_rd.set_attribute("faust_agent_submit_url",  "gram://qb1.loni.org/jobmanager-fork");
-  //queenbee_rd.set_attribute("faust_agent_binary_path", "/work/oweidner/FAUST/agent/faust_agent");	
-  //queenbee_rd.set_attribute("saga_root_path",          "/work/oweidner/megajobs");
-    
-  queenbee_rd.set_attribute("faust_agent_submit_url",  "fork://localhost/");
-  queenbee_rd.set_attribute("faust_agent_binary_path", "/Users/oweidner/Work/FAUST/build/Debug/faust_agent");	
-  queenbee_rd.set_attribute("saga_root_path",          "/usr/local/saga-1.1/");
-  
-  env.push_back("LD_LIBRARY_PATH=/usr/local/compilers/GNU/gcc-4.2.0/lib64:/home/packages/globus/globus-4.0.8-r2/lib");
+  env.push_back("LD_LIBRARY_PATH=/usr/local/compilers/GNU/gcc-4.2.0/lib64:/usr/local/packages/jdk1.6.0_06/lib:/usr/local/packages/mvapich-1.0-intel10.1/lib:/usr/local/compilers/Intel/intel_fc_10.1/lib:/usr/local/compilers/Intel/intel_cc_10.1/lib:/usr/local/compilers/Intel/mkl-10.0/lib/em64t:/home/packages/globus/globus-4.0.8-r2/lib:/work/oweidner/megajobs/lib/");
   queenbee_rd.set_vector_attribute("environment", env);
   
-  queenbee_rd.set_vector_attribute("dir_id", dir_ids);
-  queenbee_rd.set_vector_attribute("dir_path", dir_path);
-  queenbee_rd.set_vector_attribute("dir_dev_space_total_cmd", dir_dev_space_total_cmd);
-  
+  localhost_rd.set_attribute("faust_agent_submit_url",  "fork://localhost/");
+  localhost_rd.set_attribute("faust_agent_binary_path", "/Users/oweidner/Work/FAUST/build/Debug/faust_agent");	
+  localhost_rd.set_attribute("saga_root_path",          "/usr/local/saga-1.1/");
+    
   while(1) {
     sleep(5);
     std::cout << std::endl;
-    faust::resource queenbee(queenbee_rd, false);
-    queenbee.set_persistent(false);
+    
+    faust::resource queenbee  (queenbee_rd, false);
+    //faust::resource localhost (localhost_rd, false);
     
     sleep(5);
     
