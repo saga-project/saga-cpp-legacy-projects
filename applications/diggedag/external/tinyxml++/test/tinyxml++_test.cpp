85a6f8ed97058266c74c2a95d73e162fe30efc32
amerzky <amerzky@defb5e50-622e-49ec-a68e-d72c7db87b45>
2009-04-23 18:59:44 +0000

    fixing test, movig docs A
    git-svn-id: file://localhost/tmp/svn2git/svn@1193 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/diggedag/external/tinyxml++/test/tinyxml++_test.cpp
+++ applications/diggedag/external/tinyxml++/test/tinyxml++_test.cpp
@@ -19,33 +19,56 @@ int main (int argc, char** argv)
     doc.LoadFile ();
 
     // get the top adag element
-    ticpp::Element * top = doc.FirstChildElement ("adag");
+    ticpp::Element * adag = doc.FirstChildElement ("adag");
 
 
     // list nodes
     ticpp::Iterator <ticpp::Element> job ("job"); 
 
-    for ( job = job.begin (top); job != job.end (); job++ )
+    for ( job = job.begin (adag); job != job.end (); job++ )
     {
-      std::cout << (*job) << std::endl;
+      std::string s_id        = job->GetAttributeOrDefault ("id",        "Unknown");
+      std::string s_namespace = job->GetAttributeOrDefault ("namespace", "Unknown");
+      std::string s_name      = job->GetAttributeOrDefault ("name",      "Unknown");
+      std::string s_version   = job->GetAttributeOrDefault ("version",   "Unknown");
+      std::string s_runtime   = job->GetAttributeOrDefault ("runtime",   "Unknown");
+
+      std::cout << "job [" << s_id << " - " << s_name << "]" << std::endl;
+
+      ticpp::Iterator <ticpp::Element> uses ("uses"); 
+
+      for ( uses = uses.begin (job.Get ()); uses != uses.end (); uses++ )
+      {
+        std::string s_file     = uses->GetAttributeOrDefault ("file",     "Unknown");
+        std::string s_link     = uses->GetAttributeOrDefault ("link",     "Unknown");
+        std::string s_register = uses->GetAttributeOrDefault ("register", "Unknown");
+        std::string s_transfer = uses->GetAttributeOrDefault ("transfer", "Unknown");
+        std::string s_optional = uses->GetAttributeOrDefault ("optional", "Unknown");
+        std::string s_type     = uses->GetAttributeOrDefault ("type",     "Unknown");
+        std::string s_size     = uses->GetAttributeOrDefault ("size",     "Unknown");
+
+        std::cout << "  uses " << s_link << "\t: " << s_file << std::endl;
+      }
     }
     
 
     // list edges
     ticpp::Iterator <ticpp::Element> child ("child"); 
 
-    for ( child = child.begin (top); child != child.end (); child++ )
+    for ( child = child.begin (adag); child != child.end (); child++ )
     {
-      std::cout << "job" << std::endl;
-
-      ticpp::Iterator <ticpp::Attribute> a;
-
-      for ( a = a.begin (element) ; a != a.end (); a++ )
-      {
-        std::cout << " \t " << (*a) << std::endl;
-      }
+      std::string s_ref  = child->GetAttributeOrDefault ("ref", "Unknown");
       
-      std::cout << std::endl;
+      std::cout << "child [" << s_ref << "]" << std::endl;
+
+      ticpp::Iterator <ticpp::Element> parent ("parent"); 
+
+      for ( parent = parent.begin (child.Get ()); parent != parent.end (); parent++ )
+      {
+        std::string s_pref  = parent->GetAttributeOrDefault ("ref", "Unknown");
+      
+        std::cout << "  parent [" << s_pref << "]" << std::endl;
+      }
     }
     
   }
