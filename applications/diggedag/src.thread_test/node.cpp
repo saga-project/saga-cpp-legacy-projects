813fd69289ba8b043ecfa115992293d46e7fed48
amerzky <amerzky@defb5e50-622e-49ec-a68e-d72c7db87b45>
2009-04-29 18:16:05 +0000

    snap
    git-svn-id: file://localhost/tmp/svn2git/svn@1220 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/diggedag/src.thread_test/node.cpp
+++ applications/diggedag/src.thread_test/node.cpp
@@ -1,222 +1,65 @@
 
-#include <vector>
-
-#include <saga/saga.hpp>
-
-#include "util/split.hpp"
-#include "util/thread.hpp"
-#include "util/scoped_lock.hpp"
+#include <boost/bind.hpp>
 
 #include "node.hpp"
 
 
 namespace diggedag
 {
-  node::node (diggedag::node_description & nd)
-    : nd_    (nd)
-    , name_  ("")
-    , state_ (diggedag::Pending)
+  class node;
+  extern "C"
   {
-    // std::cout << "create node " << std::endl;
+    void thread_startup (void * arg)
+    {
+      diggedag::node * n = (diggedag::node *) arg;
+      n->thread_work ();
+    }
   }
 
-  node::node (std::string cmd)
-    : name_  ("")
-    , state_ (diggedag::Pending)
-  {
-    // std::cout << "create node " << std::endl;
-
-    // parse cmd into node description
-    std::vector <std::string> elems = diggedag::split (cmd);
-
-    nd_.set_attribute ("Executable", elems[0]);
-
-    elems.erase (elems.begin ());
-
-    nd_.set_vector_attribute ("Arguments", elems);
-  }
-
-  node::~node (void)
-  {
-    // std::cout << "delete node " << std::endl;
-  }
-
-  void node::set_name (const std::string name)
-  {
-    name_ = name;
-  }
-
-  void node::add_edge_in (diggedag::edge * e)
-  {
-    // std::cout << "edge-i node " << name_ 
-    //           << " : "  << e.get_src () 
-    //           << " -> " << e.get_tgt () << std::endl;
-
-    edge_in_.push_back (e);
-  }
-
-  void node::add_edge_out (diggedag::edge * e)
-  {
-    // std::cout << "edge-o node " << name_ 
-    //           << " : "  << e.get_src () 
-    //           << " -> " << e.get_tgt () << std::endl;
-
-    edge_out_.push_back (e);
-  }
-
-  // ensure the node application is run.  Before doing that, the input data
-  // edges need to be Ready, to ensure that input data are available for the
-  // application.  If they are not ready, fire has no effect.
   void node::fire (void)
   {
-    std::cout << "fire   node " << name_ << std::endl;
+    state_ = Running;
 
-    // ### scheduler hook
-    scheduler_->hook_node_run_pre (dag_, this);
+    thread_ = boost::thread (boost::bind (&diggedag::thread_startup, this));
+  }
 
-    // check if all input data are ready
-    for ( unsigned int i = 0; i < edge_in_.size (); i++ )
+
+  void node::thread_work (void)
+  {
+    try
     {
-      if ( Ready != edge_in_[i]->get_state () )
+      saga::job::description jd (jd_);
+
+      saga::job::service js;
+      saga::job::job j = js.create_job (jd);
+
+      j.run  ();
+      j.wait ();
+
+      if ( j.get_state () != saga::job::Done )
       {
-        std::cout << "       node " << name_ << " : edge " 
-          << edge_in_[i]->get_src () << "->" 
-          << edge_in_[i]->get_tgt () << " is not ready - cancel fire" << std::endl;
+        std::cout << " job failed - cancel" << std::endl;
+
+        state_ = Failed;
+
         return;
       }
       else
       {
-        std::cout << "       node " << name_ << " : edge " 
-          << edge_in_[i]->get_src () << "->" 
-          << edge_in_[i]->get_tgt () << " is ready" << std::endl;
+        std::cout << " job done" << std::endl;
       }
     }
-
-    // check if node was started before (!Pending).  
-    // If not, mark that we start the work (Running)
+    catch ( const saga::exception & e )
     {
-      my_scoped_lock l (mtx_);
+      std::cout << " job execution threw exception - cancel\n"
+                << e.what () << std::endl;
 
-      if ( Pending != state_ )
-        return;
+      state_ = Failed;
 
-      // we have work to do...
-      state_ = Running;
+      return;
     }
 
-    // all input edges are Ready, i.e. all input data are available.  We
-    // can thus really execute the node application.
-    //
-    // So: run the application, in extra thread
-    thread_run ();
-  }
-
-
-  // thread_work is called when the node is fired, and all prerequesites
-  // are fullfilled, i.e. all input data are available.  First we execute
-  // the node's application, then we fire all outgoing edges, to get data
-  // staged out.
-  void node::thread_work (void)
-  {
-    // ### scheduler hook
-    scheduler_->hook_node_run_pre (dag_, this);
-
-    // TODO: run the saga job for the job description here
-
-    // FIXME: for now, we simply fake work by sleeping for some amount of
-    // time
-    {
-      try {
-        saga::job::description jd (nd_);
-
-        saga::job::service js;
-        saga::job::job j = js.create_job (jd);
-
-        j.run  ();
-        j.wait ();
-
-        if ( j.get_state () != saga::job::Done )
-        {
-          std::cout << "       node " << name_ 
-                    << " : job failed - cancel" << std::endl;
-
-          state_ = Failed;
-
-          // ### scheduler hook
-          scheduler_->hook_node_run_fail (dag_, this);
-
-          return;
-        }
-        else
-        {
-          std::cout << "       node " << name_ 
-                    << " : job done" << std::endl;
-        }
-      }
-      catch ( const saga::exception & e )
-      {
-        std::cout << "       node " << name_ 
-          << " : job execution threw exception - cancel\n"
-          << e.what () << std::endl;
-
-        state_ = Failed;
-
-        // ### scheduler hook
-        scheduler_->hook_node_run_fail (dag_, this);
-
-        return;
-      }
-    }
-
-
-    // get data staged out, e.g. fire outgoing edges
-    for ( unsigned int i = 0; i < edge_out_.size (); i++ )
-    {
-      // std::cout << "       node " << name_ << " fires edge "
-      //   << edge_out_[i].get_src () << "->" 
-      //   << edge_out_[i].get_tgt () << std::endl;
-      edge_out_[i]->fire ();
-    }
-
-
-    // when all is done, we can update the state
-    thread_lock ();
     state_ = Ready;
-    thread_unlock ();
-
-    // ### scheduler hook
-    scheduler_->hook_node_run_done (dag_, this);
-  }
-
-  std::string node::get_name (void) const
-  {
-    return name_;
-  }
-
-  diggedag::node_description node::get_description (void) const
-  {
-    return nd_;
-  }
-
-  diggedag::state node::get_state (void)
-  {
-    // check if all input data are ready
-    for ( unsigned int i = 0; i < edge_in_.size (); i++ )
-    {
-      if ( Ready != edge_in_[i]->get_state () )
-      {
-        return Incomplete;
-      }
-    }
-
-    return state_;
-  }
-
-
-  void node::set_dag (diggedag::dag * d)
-  {
-    dag_       = d;
-    scheduler_ = dag_->get_scheduler ();
   }
 
 } // namespace diggedag
--- applications/diggedag/src.thread_test/node.hpp
+++ applications/diggedag/src.thread_test/node.hpp
@@ -2,14 +2,9 @@
 #ifndef DIGGEDAG_NODE_HPP
 #define DIGGEDAG_NODE_HPP
 
-#include <vector>
-
 #include <saga/saga.hpp>
 
-#include "util/thread.hpp"
-
-#include "node.hpp"
-#include "node_description.hpp"
+#include <boost/thread.hpp>
 
 
 namespace diggedag
@@ -23,47 +18,30 @@ namespace diggedag
     Failed     = 4
   };
 
-  std::string state_to_string (state s)
-  {
-    if      ( s == Incomplete ) return "Incomplete";
-    else if ( s == Pending    ) return "Pending"   ;
-    else if ( s == Running    ) return "Running"   ;
-    else if ( s == Ready      ) return "Ready"     ;
-    else if ( s == Failed     ) return "Failed"    ;
-    else                        return "Unknown"   ;
-  }
-  class node : public diggedag::util::thread
+
+  class node 
   {
     private:
-      diggedag::node_description     nd_;       // node application to run
-
-      std::vector <diggedag::edge *> edge_in_;  // input  data
-      std::vector <diggedag::edge *> edge_out_; // output data
-
-      std::string                    name_;     // instance name
-      diggedag::state                state_;    // instance state
-
-      diggedag::dag                * dag_;
-      diggedag::scheduler          * scheduler_;
-
+      saga::job::description         jd_;
+      diggedag::state                state_;
+      boost::mutex                   mtx_;
+      boost::thread                  thread_;
 
     public:
-      node  (diggedag::node_description & nd);
-      node  (std::string                  cmd);
-      ~node (void);
+      node  (saga::job::description & jd)
+        : jd_    (jd),
+          state_ (diggedag::Pending) 
+      { 
+      }
 
-      void            set_name        (std::string      name);
-      void            add_edge_in     (diggedag::edge * e);
-      void            add_edge_out    (diggedag::edge * e);
+      ~node (void)                   
+      {
+        thread_.join ();
+      }
 
       void            fire            (void);
       void            thread_work     (void);
-      std::string     get_name        (void) const;
-      diggedag::node_description 
-                      get_description (void) const;
       diggedag::state get_state       (void);
-
-      void            set_dag         (diggedag::dag  * d);
   };
 
 } // namespace diggedag
