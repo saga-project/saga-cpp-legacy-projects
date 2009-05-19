d5636606a611376e5547ba76c099f65b28359843
cmicel1 <cmicel1@defb5e50-622e-49ec-a68e-d72c7db87b45>
2009-05-19 17:46:13 +0000

    whoops
    git-svn-id: file://localhost/tmp/svn2git/svn@1265 defb5e50-622e-49ec-a68e-d72c7db87b45

---
+++ applications/AllPairs/source/worker/RunStaging.hpp
@@ -0,0 +1,27 @@
+//  Copyright (c) 2008 Michael Miceli and Christopher Miceli
+// 
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying 
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef AP_RUN_STAGING_HPP
+#define AP_RUN_STAGING_HPP
+
+#include <saga/saga.hpp>
+#include "../utils/LogWriter.hpp"
+
+namespace AllPairs {
+   class RunStaging {
+     public:
+      RunStaging(const std::vector<std::string> &hosts,
+                 const std::string &location, AllPairs::LogWriter *log);
+      std::vector<int> getResults();
+      ~RunStaging();
+
+     private:
+      std::vector<std::string> hosts_;
+      std::string              location_;
+      AllPairs::LogWriter     *log_;
+   }; // class RunStaging
+} // namespace AllPairs
+
+#endif // MR_RUN_STAGING_HPP
