3fe43b5e8ceb1f6c5aaefd9472efdcaf396fcbaf
oweidner <oweidner@defb5e50-622e-49ec-a68e-d72c7db87b45>
2009-06-05 19:08:57 +0000

    added missing configure flag for boost
    git-svn-id: file://localhost/tmp/svn2git/svn@1288 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/mephisto/mephisto.pl
+++ applications/mephisto/mephisto.pl
@@ -136,6 +136,7 @@ sub pull_package {
     if ( $package[0] eq "BOOST" ) {
 	# make sure that boost uses this python version
         push( @configure_cmd, "--with-python-root=$meph_install_dir" );
+	push( @configure_cmd, "--with-python=$meph_install_dir/bin/python" );
 	# workaround for bug in 1.39 bootstrap
 	push( @configure_cmd, "--libdir=$meph_install_dir/lib");
     }
