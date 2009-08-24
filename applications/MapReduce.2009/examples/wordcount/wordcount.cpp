36cc23db224026f1efca578b2d13b7c04c85f766
amerzky <amerzky@defb5e50-622e-49ec-a68e-d72c7db87b45>
2009-08-24 18:57:00 +0000

    main was is still throwing *sigh* - got rid of that. A
    git-svn-id: file://localhost/tmp/svn2git/svn@1661 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/MapReduce.2009/examples/wordcount/wordcount.cpp
+++ applications/MapReduce.2009/examples/wordcount/wordcount.cpp
@@ -43,27 +43,38 @@ REGISTER_REDUCER_CLASS(WordCountReduce, 1);
 
 
 int main(int argc, char** argv) {
-  // Will not continue execution if we are supposed to be a worker.
-  // Otherwise jobs will be submitted.
-  // TODO(miklos): the below should be done by the registerer macro.
-  mapreduce::MapRunner* runner = mapreduce::MapRunnerFactory::get_by_key("WordCountMap");
-  assert(runner);
-  mapreduce::ReduceRunner* runner2 = mapreduce::ReduceRunnerFactory::get_by_key("WordCountReduce");
-  assert(runner2);
-  mapreduce::InitFramework(argc, argv);
 
-  MapReduceResult result;
-  JobDescription job;
-  // Specify input.
-  job.set_input_format("Text");
-  FileInputFormat::AddInputPath(job,
-    "file://localhost//home/miklos/test-data/file.txt");
-  job.set_mapper_class("WordCountMap");
-  job.set_reducer_class("WordCountReduce");
-  job.set_output_format("SequenceFile");
-  job.set_num_reduce_tasks(1);
-  mapreduce::master::DistributedJobRunner job_runner(job);
-  job_runner.Initialize(
-    mapreduce::g_command_line_parameters["config"].as<std::string>());
-  job_runner.Run(&result);
+  try {
+    // Will not continue execution if we are supposed to be a worker.
+    // Otherwise jobs will be submitted.
+    // TODO(miklos): the below should be done by the registerer macro.
+    mapreduce::MapRunner* runner = mapreduce::MapRunnerFactory::get_by_key("WordCountMap");
+    assert(runner);
+    mapreduce::ReduceRunner* runner2 = mapreduce::ReduceRunnerFactory::get_by_key("WordCountReduce");
+    assert(runner2);
+    mapreduce::InitFramework(argc, argv);
+
+    MapReduceResult result;
+    JobDescription job;
+    // Specify input.
+    job.set_input_format("Text");
+    FileInputFormat::AddInputPath(job,
+      "file://localhost//home/miklos/test-data/file.txt");
+    job.set_mapper_class("WordCountMap");
+    job.set_reducer_class("WordCountReduce");
+    job.set_output_format("SequenceFile");
+    job.set_num_reduce_tasks(1);
+    mapreduce::master::DistributedJobRunner job_runner(job);
+    job_runner.Initialize(
+      mapreduce::g_command_line_parameters["config"].as<std::string>());
+    job_runner.Run(&result);
+  }
+  catch ( const std::exception & e )
+  {
+    std::cerr << "std exception cought: " << e.what () << std::endl;
+  }
+  catch ( const saga::exception & se )
+  {
+    std::cerr << "saga exception cought: " << se.what () << std::endl;
+  }
 }
