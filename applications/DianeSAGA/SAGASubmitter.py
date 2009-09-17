abcd9bb0b22fb20350f4f384d3abd8812bc96b47
oweidner <oweidner@defb5e50-622e-49ec-a68e-d72c7db87b45>
2009-09-17 08:31:27 +0000

    - consistent naming - minor bug fixes - new wrapper for remote execution
    git-svn-id: file://localhost/tmp/svn2git/svn@1725 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/DianeSAGA/SAGASubmitter.py
+++ applications/DianeSAGA/SAGASubmitter.py
@@ -8,6 +8,7 @@ import os
 import diane
 import saga
 
+
 class SAGASubmitter(Submitter):
 
     def submit_worker(self,j):
@@ -15,7 +16,21 @@ class SAGASubmitter(Submitter):
         """This function uses Ganga.GPI to submit the job created by the submitter script."""
         j.application = Ganga.GPI.Executable() 
         wf = tempfile.NamedTemporaryFile(mode="w",prefix='diane_worker_wrapper-')
-        wf.write(self.wrapper)
+        
+        # if diane dir is not explicitly given, default to 
+        # the master wrapper
+        if(self.options.dianedir == ''):
+            wf.write(self.wrapper)
+
+        # if it is given, use a modified wrapper script
+        else:
+            sh_run_wrapper = '''#!/bin/sh
+                                $('''+self.options.dianedir+'''/bin/diane-env)
+                                diane-worker-start $*
+                             '''
+ 
+            wf.write(sh_run_wrapper)
+
         wf.flush()
         uuid_path = tempfile.mkdtemp()
         uuid = open(os.path.join(uuid_path, 'ganga_job_uuid'), 'w')
@@ -41,12 +56,12 @@ class SAGASubmitter(Submitter):
         
         # works only for local repository type
         j.application.env = {'GANGA_JOB_ID': "%s@%s:%s:%d"%(ganga_config.Configuration.user,ganga_config.System.GANGA_HOSTNAME, ganga_config.Configuration.gangadir, j.id),
-                             'GANGA_JOB_UUID': str(j.info.uuid) }
+                             'GANGA_JOB_UUID': str(j.info.uuid)}
         
         j.inputsandbox = j.inputsandbox + [Ganga.GPI.File(self.master_oid_filename),
                                            Ganga.GPI.File(self.config_file.name),
                                            Ganga.GPI.File(uuid.name)]
-        j.name = "DIANEWorkerAgent"
+        j.name = "SAGA/DIANEWorkerAgent"
 
         if self.options.ENABLE_DOCTOR:
             j.outputsandbox = j.outputsandbox + ['*DUMMY_PLATFORM_STRING*']
@@ -62,6 +77,7 @@ class SAGASubmitter(Submitter):
 prog = SAGASubmitter()
 
 prog.parser.description = "Submit worker using the GangaSAGA backend. "+prog.parser.description
+prog.parser.add_option("--dianedir",       dest="dianedir",        type="string", default='',help="Diane root directory on the worker machine")
 prog.parser.add_option("--jobservice_url", dest="saga_job_service",type="string", default='',help="SAGA Job Service Endpoint URL")
 prog.parser.add_option("--filesystem_url", dest="saga_filesystem", type="string", default='',help="SAGA File System Endpoint URL")
 prog.parser.add_option("--queue",          dest="saga_queue",      type="string", default='',help="Queue name that should be used for submission")
@@ -85,7 +101,7 @@ for i in range(prog.options.N_WORKERS):
         j.backend.queue = prog.options.saga_queue 
 
     if prog.options.saga_allocation != '':
-        j.backend.job_project = prog.options.saga_allocation 
+        j.backend.allocation = prog.options.saga_allocation 
 
     prog.submit_worker(j)
 
