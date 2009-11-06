ad37657f28a7e859194125d3e3de5d1a1b0c0687
mmicel2 <mmicel2@defb5e50-622e-49ec-a68e-d72c7db87b45>
2009-11-06 19:35:53 +0000

    Create the xml files
    git-svn-id: file://localhost/tmp/svn2git/svn@1784 defb5e50-622e-49ec-a68e-d72c7db87b45

---
+++ applications/AllPairs/samples/createXML.cpp
@@ -0,0 +1,63 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int getdir (string dir, vector<string> &files)
+{
+    DIR *dp;
+    struct dirent *dirp;
+    if((dp  = opendir(dir.c_str())) == NULL) {
+        cout << "Error(" << errno << ") opening " << dir << endl;
+        return errno;
+    }
+
+    while ((dirp = readdir(dp)) != NULL) {
+        string temp(string(dirp->d_name));
+        if(temp.compare(".") == 0 || temp.compare("..") == 0 || temp[0] == '.' || temp.compare("upload.sh") == 0) {
+        } else {
+           files.push_back(string(dirp->d_name));
+        }
+    }
+    sort(files.begin(), files.end());
+    closedir(dp);
+    return 0;
+}
+
+void my_print(string i)
+{
+   if(i.compare(".") == 0 || i.compare("..") == 0) {
+   } else {
+      cout << i << endl;
+   }
+}
+
+int main(int argc, char **argv)
+{
+   vector<string> list;
+   string scheme("file://localhost/");
+   if(argc < 2)
+   {
+      cout << "Usage: ./createXML folder-to-data-files" << endl;
+      return -1;
+   }
+   string dir_name(argv[1]);
+   if(getdir(dir_name, list) != 0) {
+      cerr << "Unable to find directory" << endl;
+   }
+   else {
+      for(vector<string>::const_iterator it = list.begin(); it != list.end(); it++) {
+         cout << "   <CompareAssignment>" << endl;
+         for(vector<string>::const_iterator jt = list.begin(); jt != list.end(); jt++) {
+               cout << "     <Compare source=\"" << scheme << dir_name + "/" + *it << "\"" << endl;
+               cout << "              target=\"" << scheme << dir_name + "/" + *jt << "\"/>" << endl;
+         }
+         cout << "   </CompareAssignment>" << endl;
+      }
+   }
+   return 0;
+}
