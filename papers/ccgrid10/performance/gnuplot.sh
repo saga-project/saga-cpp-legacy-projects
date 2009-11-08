9b3bb7925571c055d28e59e2faa4f27bcd042170
aluckow <aluckow@defb5e50-622e-49ec-a68e-d72c7db87b45>
2009-11-08 18:56:30 +0000

    Some first performance data
    git-svn-id: file://localhost/tmp/svn2git/svn@1794 defb5e50-622e-49ec-a68e-d72c7db87b45

---
+++ papers/ccgrid10/performance/gnuplot.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+# set term postscript eps enhanced linewidth 1.42
+#set term postscript eps enhanced linewidth 1.42 "Helvetica" 17 
+set term postscript eps enhanced color linewidth 1.42 "Helvetica" 17 
+#set term postscript eps enhanced linewidth 1.42 "Helvetica" 17 
+
+set encoding iso_8859_1
+
+#set title "Submission Times"
+set ylabel "Runtime (in s)" #font "Helvetica, 20"
+set xlabel ""
+set size 0.8,0.8
+set lmargin 9 
+set bmargin 3
+set rmargin 2
+set tmargin 1
+                 
+##########################################################################
+# NAMD Runtimes    
+set output "namd_run.eps"       
+#set logscale x 2
+#set logscale x                                 
+set key
+set xtics autofreq
+set yrange [70:380] 
+#set xrange [2:16]
+set xrange [7:33]        
+#set xtics (2,4,8,16)
+#set logscale x
+set ylabel "Runtime (in s)" #font "Helvetica, 20"
+set xlabel "Number of Cores"   
+
+plot "namd_run.txt" using 1:2 title "QB" with lp,\
+     "namd_run.txt" using 1:3 title "Poseidon" with lp,\
+     "namd_run.txt" using 1:4 title "Nimbus" with lp,\
+     "namd_run.txt" using 1:5 title "EC2 (m2.4xlarge)" with lp 9,\
+     "namd_run.txt" using 1:6 title "EC2 (m1.large)" with lp 14  
+
+##########################################################################
+# Scenarios Times
+# Workload 50 Jobs Effect of Offloading to Cloud 
+#set output "setup_time_core.eps"
+#set xrange [0:32]
+#set yrange [0:600]      
+#plot "setup_time_core.txt" using 1:2 title "EC2 (m1.large)" with lp,\
+#     "setup_time_core.txt" using 1:4 title "Nimbus" with lp,\
+#     "setup_time_core.txt" using 1:5 title "Poseidon" with lp 9
+#
+##########################################################################
+# Setup Times
+
+set nokey
+#set xdata time
+#set timefmt "%d.%m%y"
+#set format x "%d.%m."
+#set format y "%.0f"
+set xrange [0.5:3.5] 
+set yrange [0:700] 
+#set boxwidth 0.8 relative
+#set style fill solid 0.9
+# set linestyle 1 lt 1 lw 50
+set output "setup_time.eps"  
+set xtics ("EC2" 1, "Nimbus" 2.0, "Poseidon" 3.0, "QB" 4.0)
+#plot "setup_time.txt" using 1:2:(0.7) title "Setup Times" with boxes linetype 1  fs solid 0.25
+
+set boxwidth 0.8 relative
+set style fill solid 0.9
+plot "setup_time.txt" using 1:2:3:(0.75)  title "Setup Times" with boxerrorbars linetype 1  fs solid 0.5
