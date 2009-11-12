7ac598d0adc00fa1a0b69d8fa1eb0fd46215fd5d
svn_miklos <svn_miklos@defb5e50-622e-49ec-a68e-d72c7db87b45>
2009-11-12 18:58:27 +0000

    Added missing license header.
    git-svn-id: file://localhost/tmp/svn2git/svn@1834 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/MapReduce.2009/source/mapreduce/worker/MergingRawRecordReader.hpp
+++ applications/MapReduce.2009/source/mapreduce/worker/MergingRawRecordReader.hpp
@@ -1,3 +1,8 @@
+//  Copyright (c) 2009 Miklos Erdelyi
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying 
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef MAPREDUCE_WORKER_MERGINGRAWRECORDREADER_
 #define MAPREDUCE_WORKER_MERGINGRAWRECORDREADER_
 
