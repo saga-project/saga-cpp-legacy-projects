4021f1c88cbab1aedc15d80684d6755b2887044e
mmicel2 <mmicel2@defb5e50-622e-49ec-a68e-d72c7db87b45>
2009-12-21 02:33:15 +0000

    Got to compile on my machine.  Needed to slightly alter a bib entry
    git-svn-id: file://localhost/tmp/svn2git/svn@2076 defb5e50-622e-49ec-a68e-d72c7db87b45

--- papers/data_intensive/eScience09/make.sh
+++ papers/data_intensive/eScience09/make.sh
@@ -2,4 +2,4 @@
 latex data_intensive_paper
 bibtex data_intensive_paper
 latex data_intensive_paper
-latex data_intensive_paper && dvipdf data_intensive_paper.dvi && evince data_intensive_paper.pdf
+latex data_intensive_paper && evince data_intensive_paper.pdf
