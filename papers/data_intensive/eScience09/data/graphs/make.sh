cbce5032988de97114b2a46b3dcc9ff0a8ec491d
cmicel1 <cmicel1@defb5e50-622e-49ec-a68e-d72c7db87b45>
2010-01-04 02:41:21 +0000

    easier gnuplot working with
    git-svn-id: file://localhost/tmp/svn2git/svn@2135 defb5e50-622e-49ec-a68e-d72c7db87b45

---
+++ papers/data_intensive/eScience09/data/graphs/make.sh
@@ -0,0 +1,16 @@
+gnuplot < CloudStoreComputeMake.gnuplot
+gnuplot < CloudStoreMake.gnuplot
+gnuplot < CloudStoreNoComputeMake.gnuplot
+gnuplot < ConventionalMake.gnuplot
+gnuplot < IntelligentExtremes.gnuplot
+gnuplot < IntelligentVsConventionalMake.gnuplot
+gnuplot < LocalFigure.gnuplot
+gnuplot < StagingAsAPortionOfIntelligence.gnuplot
+epstopdf CloudStoreCompute.eps
+epstopdf CloudStoreFigure.eps
+epstopdf CloudStoreNoComputeSmallerDataSet.eps
+epstopdf ConventionalFigure.eps
+epstopdf IntelligentExtremes.eps
+epstopdf IntelligentVsConventionalFigure.eps
+epstopdf LocalFigure.eps
+epstopdf StagingAsAPortionOfIntelligenceFigure.eps
