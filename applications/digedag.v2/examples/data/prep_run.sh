72401d6390bdcdf52d87c6667edc52e33190108e
amerzky <amerzky@defb5e50-622e-49ec-a68e-d72c7db87b45>
2010-03-10 10:53:17 +0000

    adding a script which prepares /tmp/0 for a run.  first arg should be the name of the data set, 'm101.2mass.1.1.0.000278' being the default. Probably not very useful, but anyway... :-P A
    git-svn-id: file://localhost/tmp/svn2git/svn@2428 defb5e50-622e-49ec-a68e-d72c7db87b45

---
+++ applications/digedag.v2/examples/data/prep_run.sh
@@ -0,0 +1,49 @@
+#!/bin/sh 
+
+if test "x$MONTAGE_LOCATION" = "x"; then
+  echo "MONTAGE_LOCATION not set"
+  exit -1
+fi
+
+if ! test -d $MONTAGE_LOCATION/bin; then
+  echo "MONTAGE_LOCATION/bin does not exist"
+  exit -2
+fi
+
+src=$1
+data=`pwd`
+
+if test "x$src" = "x"; then
+  src=m101.2mass.1.1.0.000278
+fi
+
+if ! test -d $data/$src/data; then
+  echo data directory not found: $data/$src/data
+  exit -3
+fi
+
+echo "preparing $data/$src/data"
+read -p "continue? <Y|n> : " answer
+
+if test "x$answer" = "x"; then
+  answer=y
+fi
+
+if ! test "x$answer" = "xy"; then
+  echo "abort ($answer)"
+  exit -4
+fi
+
+
+cd /tmp
+
+rm -f 0
+ln -s $data/$src/data 0
+
+cd 0
+
+rm -f bin
+ln -s $MONTAGE_LOCATION/bin .
+
+cd $data
+
