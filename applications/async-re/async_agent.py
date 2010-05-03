59ee50e1bc4c5092465701a9863096744fa4f7fb
athota1 <athota1@defb5e50-622e-49ec-a68e-d72c7db87b45>
2010-05-03 21:10:37 +0000

    the async_agent.py, different combinations tried
    git-svn-id: file://localhost/tmp/svn2git/svn@2627 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/async-re/async_agent.py
+++ applications/async-re/async_agent.py
@@ -2,18 +2,21 @@ import saga
 import os
 
 if __name__ == "__main__":
-
  try:
+  os.environ['PBS_NODEFILE']='/work/athota1/machinefile'
+  os.putenv("MACHINEFILE", "/work/athota1/machinefile")
+  os.system("export PBS_NODEFILE=/work/athota1/machinefile")
+  print os.system("env")
   jd = saga.job.description()
-  jd.executable = "/bin/date"
-  jd.number_of_processes = "1"
-  jd.spmd_variation = "single"
- # jd.arguments = ["NPT.conf"]
-  jd.arguments = [""]
+  #jd.executable = "/bin/date"mpirun -np xx -machinefile $MACHINEFILE <path to namd>/namd2 NPT.con
+  jd.executable = "mpirun"
+  #jd.number_of_processes = "1"
+ # jd.spmd_variation = "single"
+  jd.arguments = ["-np 4 -machinefile /work/athota1/machinefile namd2 NPT.conf"]
   jd.working_directory ="/work/athota1/new_bigjob/bigjob/"
  # jd.job_project = ["loni_jhabig09"]
   jd.output = "stdout.txt"
-#  jd.wall_time_limit = "15"
+ # jd.wall_time_limit = "15"
   jd.error = "stderr.txt"
   js = saga.job.service("fork://localhost")
   job = js.create_job(jd)
