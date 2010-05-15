2f2a5d19c0f63b5511b4c31e4a099b3bdac256fa
athota1 <athota1@defb5e50-622e-49ec-a68e-d72c7db87b45>
2010-05-15 08:33:23 +0000

    some changes
    git-svn-id: file://localhost/tmp/svn2git/svn@2653 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/async-re/async_agent.py
+++ applications/async-re/async_agent.py
@@ -1,13 +1,18 @@
 import saga
 import os
 import subprocess
+import logging
 #if __name__ == "__main__":
 # try:
-   #command = "mpirun" + " -np " + "4" + " -machinefile " + "/work/athota1/machinefile" + " " + "/work/athota1/new_bigjob/bigjob/namd2" + " /work/athota1/new_bigjob/bigjob/NPT.conf"
+command = "mpirun" + " -np " + "4" + " -machinefile " + "/work/athota1/machinefile" + " " + "/work/athota1/new_bigjob/bigjob/namd2" + " /work/athota1/new_bigjob/bigjob/NPT.conf"
   
   # print "execute: " + command + " in " + "workingdirectory" + " from: " + str(socket.gethostname()) + " (Shell: " +")"
+LOG_FILENAME = '/work/athota1/logging_example.out'
+logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
 # bash works fine for launching on QB but fails for Abe :-(
-subprocess.Popen('echo "hello"', shell=True, executable="/bin/bash")
-#subprocess.Popen(args=command, executable="/bin/bash", stderr="stderr",stdout="stdout",cwd=os.getcwd(),shell=True)
+logging.debug('parsing')
+#subprocess.Popen('echo "hello" > /work/athota1/jump.txt', shell=True, executable="/bin/bash")
+logging.debug('not parsed')
+subprocess.Popen(args=command, executable="/bin/bash", stderr=/work/athota1/stderr,stdout=/work/athota1/stdout,cwd=os.getcwd(),shell=True)
 # except e: 
 #   print str(e)
