186a87b9b22618280b38a5b568930f25e63ccd1b
athota1 <athota1@defb5e50-622e-49ec-a68e-d72c7db87b45>
2010-07-01 05:35:26 +0000

    adding some decentralized files
    git-svn-id: file://localhost/tmp/svn2git/svn@2752 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/async-re/decentralized/async_agent.py
+++ applications/async-re/decentralized/async_agent.py
@@ -2,17 +2,29 @@ import saga
 import os
 import subprocess
 import logging
-#if __name__ == "__main__":
-# try:
-command = "mpirun" + " -np " + "4" + " -machinefile " + "/work/athota1/machinefile" + " " + "/work/athota1/new_bigjob/bigjob/namd2" + " /work/athota1/new_bigjob/bigjob/NPT.conf"
-  
+import sys
+
+num = sys.argv[2]
+command = "mpirun" + " -np " + "16" + " -machinefile " + "/work/athota1/machinefile" + " " + "/work/athota1/new_bigjob/decentralized/agent/" +str(num) + "/namd2" + " /work/athota1/new_bigjob/decentralized/"+str(num)+"/NPT.conf"
+
   # print "execute: " + command + " in " + "workingdirectory" + " from: " + str(socket.gethostname()) + " (Shell: " +")"
-LOG_FILENAME = '/work/athota1/logging_example.out'
+LOG_FILENAME = '/work/athota1/new_bigjob/decentralized/logging_example-'+str(num)+'.out'
 logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
 # bash works fine for launching on QB but fails for Abe :-(
 logging.debug('parsing')
+stdout = open("/work/athota1/new_bigjob/decentralized/agent/"+str(num)+"/"+"stdout-"+str(num), "w")
+stderr = open("/work/athota1/new_bigjob/decentralized/agent/"+str(num)+"/"+"stderr-"+str(num), "w")
 #subprocess.Popen('echo "hello" > /work/athota1/jump.txt', shell=True, executable="/bin/bash")
 logging.debug('not parsed')
-subprocess.Popen(args=command, executable="/bin/bash", stderr=/work/athota1/stderr,stdout=/work/athota1/stdout,cwd=os.getcwd(),shell=True)
+#subprocess.Popen(args=command, executable="/bin/bash", stderr=stderr,stdout=stdout,cwd=os.getcwd(),shell=True)
 # except e: 
 #   print str(e)
+
+agent_url = saga.url("advert://fortytwo.cct.lsu.edu/"+"BigJob/BigJob" + "-" + sys.argv[1] + "/"+str(num)+"/")
+agent_dir = saga.advert.directory(agent_url, saga.advert.Create | saga.advert.CreateParents | saga.advert.ReadWrite)
+tring = agent_url.get_string()
+logging.debug(tring)
+agent_dir.set_attribute("state", str(saga.job.Running))
+subprocess.Popen(args=command, executable="/bin/bash", stderr=stderr,stdout=stdout,cwd=os.getcwd(),shell=True)
+#agent_dir.set_attribute("state", str(saga.job.Done))
+#print str(app_dir.get_attribute("state"))
