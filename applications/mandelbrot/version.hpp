c8b278cbf6832d1dfa94b46bc991344201b98f8f
amerzky <amerzky@defb5e50-622e-49ec-a68e-d72c7db87b45>
2010-12-07 21:14:53 +0000

    added missing file
    git-svn-id: file://localhost/tmp/svn2git/svn@3321 defb5e50-622e-49ec-a68e-d72c7db87b45

---
+++ applications/mandelbrot/version.hpp
@@ -0,0 +1,11 @@
+
+#ifndef SAGA_MANDELBROT_VERSION_HPP
+#define SAGA_MANDELBROT_VERSION_HPP
+
+// this version string is used to allow version syncing between master and
+// client.  Incompatible clients should quit when encountering a version
+// mismatch
+#define SAGA_MANDELBROT_VERSION "1.0"
+
+#endif // SAGA_MANDELBROT_VERSION_HPP
+
