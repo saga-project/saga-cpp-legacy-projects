8e827db02eb38232abf22ae76785d04432ee9326
aluckow <aluckow@defb5e50-622e-49ec-a68e-d72c7db87b45>
2011-02-04 22:05:56 +0000

    reduce script for wc pilotstore example
    git-svn-id: file://localhost/tmp/svn2git/svn@3696 defb5e50-622e-49ec-a68e-d72c7db87b45

---
+++ applications/pilotstore/src/examples/wordcount-reduce.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+# Aggregate subtotals of mapping phase
+import saga
+import sys
+sys.path.append("../../../BigJob/")
+sys.path.append("../store/")
+
+from pilotstore import *
+
+# for logging
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+APPLICATION_NAME="wordcount"
+
+lines, blanklines, sentences, words = 0, 0, 0, 0
+
+if len(sys.argv) !=4:
+    print "Usage: " + sys.argv[0] + " <pilot data> <pilot store> <application advert url>"
+    sys.exit(-1)
+
+logging.debug("Pilot Data URL: " + sys.argv[1] + " Pilot Store Name: " + sys.argv[2] + " App URL: " + sys.argv[3])
+pd = pilot_data.from_advert(saga.url(sys.argv[1]))
+
+print '-' * 50
+total_lines = 0
+total_blanklines = 0
+total_sentences = 0
+total_words = 0
+for ps in pd.list_pilot_store():
+    if ps.name == sys.argv[2]:
+        logging.debug("open pilot store: " + ps.name)
+        for file in ps.list_files():
+            try:
+                # use a text file you have, or google for this one ...
+                saga_url = saga.url(file.get_string())
+                filename = saga_url.path
+                logging.debug("open file: " + filename)      
+                textf = open(filename, 'r')
+                   
+                # reads one line at a time
+                for line in textf:
+                    comp = line.partition(":")
+                    if comp[0].strip()=="Lines":
+                        total_lines = total_lines + int(comp[2])            
+                
+                textf.close()
+            except IOError:
+              print 'Cannot open file %s for reading' % filename
+              import sys
+              sys.exit(0)
+          
+logging.debug("Finished counting words")
+          
+filename = "data/result-reduce.txt"
+result_file = open(filename, 'w')
+result_file.write("Lines      : " + str(total_lines)+ '\n')
+result_file.write("Blank lines: " + str(total_blanklines)+ '\n')
+result_file.write("Sentences  : " + str(total_sentences)+ '\n')
+result_file.write("Words      : " + str(total_words)+ '\n')
+result_file.close()
+
+print "Creating pilot store for result file"
+base_dir = saga.url("file://localhost" + os.getcwd()+"/data")
+ps_results = pilot_store("reduce-results", base_dir, pd)
+pd.add_pilot_store(ps_results)
+
+for p in pd.list_pilot_store():
+    print p
