217452e796df1e7fbb2cc7d62388f23e8206fea3
amerzky <amerzky@defb5e50-622e-49ec-a68e-d72c7db87b45>
2011-04-18 10:07:57 +0000

    the torque problem seems localized to abe
    git-svn-id: file://localhost/tmp/svn2git/svn@4355 defb5e50-622e-49ec-a68e-d72c7db87b45

--- deployment/tg-csa/make.saga.csa.mk
+++ deployment/tg-csa/make.saga.csa.mk
@@ -101,7 +101,9 @@ ifeq "$(SAGA_VERSION)" "trunk"
   saga-adaptors:: saga-adaptor-bes 
   saga-adaptors:: saga-adaptor-aws 
   saga-adaptors:: saga-adaptor-drmaa
-# saga-adaptors:: saga-adaptor-torque
+ifneq "$(HOSTNAME)" "abe"
+  saga-adaptors:: saga-adaptor-torque
+endif
   saga-adaptors:: saga-adaptor-pbspro
   saga-adaptors:: saga-adaptor-condor
 endif
