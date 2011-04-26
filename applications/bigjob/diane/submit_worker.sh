9210112e2b68ee9be3e94929fdd59544e37178eb
marksant <marksant@defb5e50-622e-49ec-a68e-d72c7db87b45>
2011-04-26 20:30:39 +0000

    Wrapper scripts that launches a new diane worker agent.
    git-svn-id: file://localhost/tmp/svn2git/svn@4483 defb5e50-622e-49ec-a68e-d72c7db87b45

---
+++ applications/bigjob/diane/submit_worker.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+diane-submitter SAGA \
+--jobservice-url="gram://eric1.loni.org/jobmanager-pbs" \
+--filesystem-url="gsiftp://eric1.loni.org/work/marksant/diane" \
+--diane-worker-number=1
