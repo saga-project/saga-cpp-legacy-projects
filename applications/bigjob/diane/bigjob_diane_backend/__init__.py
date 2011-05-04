a4804ecad25bce182ec580f24175653cb4550dc7
marksant <marksant@defb5e50-622e-49ec-a68e-d72c7db87b45>
2011-05-04 03:19:48 +0000

    Set an explicit bind IP address on cyder.
    Background: the onmiORB CORBA library does not get the correct order 
    of network interfaces and therefore does not bind to the default interface.
    
    
    git-svn-id: file://localhost/tmp/svn2git/svn@4501 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/bigjob/diane/bigjob_diane_backend/__init__.py
+++ applications/bigjob/diane/bigjob_diane_backend/__init__.py
@@ -7,6 +7,7 @@ giopMaxMsgSize = 19100200
 threadPerConnectionPolicy = 0
 maxServerThreadPoolSize = 150
 #dumpConfiguration = 1
+endPoint = giop:tcp:130.39.12.221:
 """
 
 default_worker = default_master
