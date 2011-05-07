48e1bbf5a874a6ca95b282177994ef6344dcadf5
marksant <marksant@defb5e50-622e-49ec-a68e-d72c7db87b45>
2011-05-07 03:51:40 +0000

    Remove static config, configuration is now by arguments.
    git-svn-id: file://localhost/tmp/svn2git/svn@4517 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/bigjob/diane/submit_worker.sh
+++ applications/bigjob/diane/submit_worker.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
+
+JOBSERVICE_URL=$1
+FILESYSTEM_URL=$2
+DIANE_WORKER_NUMBER=$3
+
+if [ -z "${JOBSERVICE_URL}" -o -z "${FILESYSTEM_URL}" -o -z "${DIANE_WORKER_NUMBER}" ]; then
+    echo "usage: submit_worker.sh <jobservice_url> <filesystem_url> <diane_worker_number>"
+    exit 1
+fi
+
 diane-submitter SAGA \
---jobservice-url="gram://eric1.loni.org/jobmanager-pbs" \
---filesystem-url="gsiftp://eric1.loni.org/work/marksant/diane" \
---diane-worker-number=1
+--jobservice-url=${JOBSERVICE_URL} \
+--filesystem-url=${FILESYSTEM_URL} \
+--diane-worker-number=${DIANE_WORKER_NUMBER}
