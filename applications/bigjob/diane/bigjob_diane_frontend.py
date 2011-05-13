07877f3669cfa838ced87e97328098445550b3ce
marksant <marksant@defb5e50-622e-49ec-a68e-d72c7db87b45>
2011-05-13 02:14:29 +0000

    - Add subjob status checking. - Move rundir code into a function, should be made proper, but at least it is   isolated now
    git-svn-id: file://localhost/tmp/svn2git/svn@4565 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/bigjob/diane/bigjob_diane_frontend.py
+++ applications/bigjob/diane/bigjob_diane_frontend.py
@@ -4,6 +4,39 @@ Bigjob Implementation with Diane backend
 
 """
 
+
+"""
+Job States
+
+* Unknown
+  Not part of the SPEC...
+
+* New
+  This state identies a newly constructed job instance which has not yet run.
+  This state corresponds to the BES state Pending. This state is initial.
+
+* Running     
+  The run() method has been invoked on the job, either explicitly or implicitly.
+  This state corresponds to the BES state Running. This state is initial.
+
+* Done    
+  The synchronous or asynchronous operation has finished successfully. It
+  corresponds to the BES state Finished. This state is final.
+
+* Canceled    
+  The asynchronous operation has been canceled, i.e. cancel() has been called on
+  the job instance. It corresponds to the BES state Canceled. This state is final.
+
+* Failed  
+  The synchronous or asynchronous operation has finished unsuccessfully. It
+  corresponds to the BES state Failed. This state is final.
+
+* Suspended   
+  Suspended identifies a job instance which has been suspended. This state
+  corresponds to the BES state Suspend. 
+
+"""
+
 import api.base
 import saga
 import os
@@ -11,6 +44,18 @@ import sys
 from stat import *
 from subprocess import *
 import shutil
+
+def get_rundir():
+    p1 = Popen(["diane-ls"], stdout=PIPE)
+    p2 = Popen(["head", "-n1"], stdin=p1.stdout, stdout=PIPE)
+    p3 = Popen(["cut", "-f1", "-d "], stdin=p2.stdout, stdout=PIPE)
+    p2.stdout.close()  # Allow p2 to receive a SIGPIPE if p2 exits.
+    p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
+    rundir = p3.communicate()[0].strip()
+
+    return rundir
+
+
 
 def get_uuid():
     wd_uuid=""
@@ -27,13 +72,14 @@ def get_uuid():
     return wd_uuid
 
 
-class bigjob(api.base.bigjob):
+class bigjob_diane(api.base.bigjob):
        
     def __init__(self, database_host=None):  
         self.pilot_url = "diane://"
         self.uuid = get_uuid()
         self.state = saga.job.Unknown
 
+        # TODO: create DIANE master object
         print "Launching Diane Master"
         os.system("~/proj/bigjob/diane/run_master.sh")
     
@@ -54,32 +100,39 @@ class bigjob(api.base.bigjob):
         if bigjob_agent_executable is not None:
             print "Agent Executable is specified, but ignoring it!"
 
+        print 'submitting worker, resource url: %s workdir: %s number of nodes: %s' % (resource_url, working_directory, number_nodes)
         os.system("~/proj/bigjob/diane/submit_worker.sh %s %s %s" % \
                 (resource_url, working_directory, number_nodes))
-                 
-         
+        self.rundir = get_rundir()
+        print 'Rundir:', self.rundir
+
+
+
     def get_state(self):        
+        # TODO
+
+                 
+        print 'Bigjob get_state:', self.state
         return self.state
+         
     
     def get_state_detail(self): 
-        pass
+        # TODO
+        return "running"
     
     def cancel(self):        
+        # TODO: kill master? agents?
         pass
                     
                     
-class subjob(api.base.subjob):
+class subjob_diane(api.base.subjob):
     
     def __init__(self, database_host=None):
         self.state = saga.job.Unknown
         self.uuid = get_uuid()
-        p1 = Popen(["diane-ls"], stdout=PIPE)
-        p2 = Popen(["head", "-n1"], stdin=p1.stdout, stdout=PIPE)
-        p3 = Popen(["cut", "-f1", "-d "], stdin=p2.stdout, stdout=PIPE)
-        p2.stdout.close()  # Allow p2 to receive a SIGPIPE if p2 exits.
-        p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
-        self.rundir = p3.communicate()[0].strip()
 
+        # TODO: this incorrectly assumes that we use the most recent rundir
+        self.rundir = get_rundir()
         print 'Rundir:', self.rundir
     
     def submit_job(self, pilot_url, jd):
@@ -98,7 +151,29 @@ class subjob(api.base.subjob):
         shutil.move(scriptname,os.path.join(self.rundir,'submitted'))
 
     def get_state(self):        
+
+        scriptname = self.uuid + '.sh'
+
+        if os.path.isfile(self.rundir + '/submitted/' + scriptname ):
+            print 'DEBUG: subjob state:', self.state
+        if os.path.isfile(self.rundir + '/scheduled/' + scriptname ):
+            self.state = saga.job.New
+            print 'DEBUG: subjob state:', self.state
+        if os.path.isfile(self.rundir + '/running/' + scriptname):
+            self.state = saga.job.Running
+            print 'DEBUG: subjob state:', self.state
+        if os.path.isfile(self.rundir + '/done/' + scriptname):
+            self.state = saga.job.Done
+            print 'DEBUG: subjob state:', self.state
+        if os.path.isfile(self.rundir + '/cancelled/' + scriptname):
+            self.state = saga.job.Canceled
+            print 'DEBUG: subjob state:', self.state
+        if os.path.isfile(self.rundir + '/failed/' + scriptname):
+            self.state = saga.job.Failed
+            print 'DEBUG: subjob state:', self.state
+            
         return self.state
     
     def delete_job(self):
+        # TODO: move and remove in directory structure
         pass
