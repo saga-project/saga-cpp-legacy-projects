cb9df723a03ee9bf81bce79741727594c40c5c53
marksant <marksant@defb5e50-622e-49ec-a68e-d72c7db87b45>
2011-05-17 05:21:08 +0000

    DIANE SAGASubmitter now supports specifying the number of nodes and/or the number of workers per node.
    git-svn-id: file://localhost/tmp/svn2git/svn@4595 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/bigjob/diane/submit_worker.sh
+++ applications/bigjob/diane/submit_worker.sh
@@ -2,14 +2,18 @@
 
 JOBSERVICE_URL=$1
 FILESYSTEM_URL=$2
-DIANE_WORKER_NUMBER=$3
+NUMBER_NODES=$3
+WORKERS_PER_NODE=$4
 
-if [ -z "${JOBSERVICE_URL}" -o -z "${FILESYSTEM_URL}" -o -z "${DIANE_WORKER_NUMBER}" ]; then
-    echo "usage: submit_worker.sh <jobservice_url> <filesystem_url> <diane_worker_number>"
+if [ -z "${JOBSERVICE_URL}" -o -z "${FILESYSTEM_URL}" -o -z \
+    "${NUMBER_NODES}" -o -z "${WORKERS_PER_NODE}" ]; then
+    echo "usage: submit_worker.sh <jobservice_url> <filesystem_url> \
+<number_nodes> <workers_per_node>"
     exit 1
 fi
 
 diane-submitter SAGA \
 --jobservice-url=${JOBSERVICE_URL} \
 --filesystem-url=${FILESYSTEM_URL} \
---diane-worker-number=${DIANE_WORKER_NUMBER}
+--diane-number-nodes=${NUMBER_NODES} \
+--diane-workers-per-node=${WORKERS_PER_NODE}
