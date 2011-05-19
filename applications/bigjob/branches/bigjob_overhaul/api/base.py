c7de51afd67b301269dfaa8aaed2d8d29cac8535
marksant <marksant@defb5e50-622e-49ec-a68e-d72c7db87b45>
2011-05-19 00:27:42 +0000

    The before-dinner-discussion version of the new Bigjob API. Feedback more than welcome! Please shoot at it! I mean it! Really! ;-)
    git-svn-id: file://localhost/tmp/svn2git/svn@4627 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/bigjob/branches/bigjob_overhaul/api/base.py
+++ applications/bigjob/branches/bigjob_overhaul/api/base.py
@@ -1,48 +1,137 @@
-""" Module API
+""" Bigjob API
 
-This Module contains the API of the BigJob framework
-
-All BigJob implementation must be derived from this API.
+This Module contains the API of the Bigjob framework.
+All Bigjob implementation must be derived from this API.
 
 """
 
-class bigjob(object):
-       
-    def __init__(self, database_host):  
-         pass
-    
-    def start_pilot_job(self, 
-                 lrms_url, 
-                 bigjob_agent_executable,
-                 number_nodes,
-                 queue,
-                 project,
-                 working_directory,
-                 userproxy,
-                 walltime,
-                 processes_per_node=1):
+#
+# Bigjob data structures
+# 
+class BigjobResource():
+    """class to pass resource descriptions values around"""
+    def __init__(self):
+        # self.name 
+        # self.type 
+        # self.url
+        # self.queue
+        # self.project
+        # self.working_directory
+        # self.walltime
+        # self.userproxy
+        # self.agent_executable
+        pass
+
+class BigjobCapabilities():
+    """class to pass resource capabilities around"""
+    def __init__(self):
+        # self.number_of_cores
+        # self.number_of_nodes
+        # self.time_to_live
+        pass
+
+#
+# Bigjob class
+#
+class Bigjob():
+    """ This class represents the Bigjob. """
+
+    def __init__(self, resource):
+        """ Create a Bigjob object. Takes a resource description as only argument. """
+        # self.__uuid
         pass
          
     def get_state(self):        
+        """ Return the state of the Bigjob. """
+        pass
+
+    def get_capabilities(self):
+        """ Return the capabilities that this Bigjob provides. """
         pass
     
-    def get_state_detail(self): 
+    def get_subjobs(self):        
+        """ Return the list of Subjobs that are submitted to this Bigjob. """
         pass
-    
+
+    def resize(self):        
+        """ (re-)Calibrate the BigJob to the given 'size'. """
+        pass
+
+    def submit_subjob(self, subjob):
+        """ Execute a Subjob to this Bigjob. """
+        pass
+
     def cancel(self):        
+        """ Cancel the BigJob and its Subjobs. """
         pass
-                    
-                    
-class subjob(object):
-    
-    def __init__(self, database_host):
+
+
+
+
+
+
+#
+# Subjob data structures
+# 
+class SubjobRequirements():
+    """ Class to pass Subjob requirements around. """
+    def __init__(self):
+        # self.number_of_cores
+        # self.minimum_runtime
         pass
-    
-    def submit_job(self, pilot_url, jd):
+
+class SubjobInfo():
+    """ Class to describe a task. """
+    def __init__(self):
+        # self.runtime
+        # self.location
+        pass
+
+class SubjobTask():
+    """ Class to describe a task. """
+    def __init__(self):
+        # self.state
+        # self.program
+        # self.inputs
+        # self.outputs
+        pass
+
+#
+# Subjob class
+# 
+class Subjob():
+    """ This class represents the Subjob concept in the Bigjob framework.
+        It is the representation of the schedulable entity.
+    """
+
+    def __init__(self, task_list, requirements):
+        # self.__uuid
+        pass
+
+    def get_requirements(self):        
+        """ Return the requirements that this Subjob has """
+        pass
+
+    def get_bigjob(self):        
+        """ Return the Bigjob where this Subjob is running """
+        pass
+
+    def set_bigjob(self):        
+        """ Set the Bigjob where this Subjob is running """
         pass
     
     def get_state(self):        
+        """ Return the (simple) state of the Subjob """
+        pass
+
+    def get_info(self):        
+        """ Return structured info about the Subjob """
         pass
     
+    def get_task_info(self):        
+        """ Return info about the individual tasks """
+        pass
+
     def cancel(self):
+        """ Cancel the Subjob and its tasks """
         pass
