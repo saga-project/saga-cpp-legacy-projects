97fd9ed60c256bbae1280ca185a8898a04d23556
marksant <marksant@defb5e50-622e-49ec-a68e-d72c7db87b45>
2011-08-31 18:58:29 +0000

    Second scenerio: # N WorkUnits, to M PilotJob, using 1 PilotJobService
    git-svn-id: file://localhost/tmp/svn2git/svn@5120 defb5e50-622e-49ec-a68e-d72c7db87b45

---
+++ applications/bigjob/branches/bigjob_overhaul/examples/example-Nwu-Mpj-1pjs.py
@@ -0,0 +1,51 @@
+#
+# This example demonstrates:
+# N WorkUnits, to M PilotJob, using 1 PilotJobService
+#
+import troy
+
+# Create our callback class and method
+class MyCallback(Callback):
+    def cb(self, wu, member, value):
+        print 'Called by', wu, 'because ', member, \
+                ' changed, and its new value is:', value
+        
+        return True # Stay registered
+
+
+# Initiate the PilotJobService
+pjs = troy.PilotJobService()
+
+# Create a PilotJob on Ranger, let the "system" decide which type
+pj1_desc = troy.PilotJobDescription()
+pj1_desc.total_cpu_count = 16
+pj1 = pjs.create('gram://ranger', pj1_desc)
+
+# Create a PilotJob on Kraken, specify that we want a DIANE backend
+pj2_desc = troy.PilotJobDescription()
+pj2_desc.total_cpu_count = 16
+pj2 = pjs.create('gram://kraken', pj2_desc, pj_type=troy.PILOTJOB_TYPE.DIANE)
+
+# Instantiate a WorkUnitService, 
+wus = troy.WorkUnitService()
+
+# Link the PilotJobService we created before
+wus.add(pjs)
+
+# Some input
+all_input_files = { '/data/file1', '/data/file2' }
+
+# Create bfast WorkUnits and submit them to the WorkUnitService
+for input in all_input_files:
+  wu_desc = troy.WorkUnitDescription()
+  wu_desc.executable = "/bin/bfast"
+  wu_desc.total_cpu_count = 4
+  wu_desc.arguments = ["match", "-t4", input]
+  wu = wus.submit(wu_desc)
+  cb = MyCallback()
+  wu.add_callback(state, cb)
+
+
+#
+# Some work, sleep, etc.
+#
