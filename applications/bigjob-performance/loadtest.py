3bd23433ebc034cd5a5b2a4897ca6e67fc0c3b76
aluckow <aluckow@defb5e50-622e-49ec-a68e-d72c7db87b45>
2011-09-25 11:55:36 +0000

    updates to perf test scripts
    git-svn-id: file://localhost/tmp/svn2git/svn@5255 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/bigjob-performance/loadtest.py
+++ applications/bigjob-performance/loadtest.py
@@ -12,17 +12,25 @@ from bigjob.bigjob_manager import bigjob, subjob
 #COORDINATION_URL = "redis://localhost"
 #COORDINATION_URL = "redis://i136"
 #COORDINATION_URL = "redis://cyder.cct.lsu.edu:8080"
-#COORDINATION_URL = "redis://cyder.cct.lsu.edu:8080"
-COORDINATION_URL = "tcp://localhost"
+#COORDINATION_URL = "tcp://localhost"
+
+NUMBER_RUNS=10
+COORDINATION_URL = ["tcp://i136", "redis://cyder.cct.lsu.edu:8080", "redis://i136","advert://advert.cct.lsu.edu:8080"]
+#COORDINATION_URL = ["advert://advert.cct.lsu.edu:8080"]
 
 #NUMBER_JOBS=[256]
-NUMBER_JOBS=[64]
-NUMBER_NODES=[2]
+# Scenario WUs
+NUMBER_JOBS=[64,128,256,512,1024,2048]
+NUMBER_NODES=[8,8,8,8,8,8]
+#NUMBER_JOBS=[64]
+# Scenario cores
+#NUMBER_JOBS=[32,64,128,256,512]
+#NUMBER_NODES=[1,2,4,8,16]
 #NUMBER_NODES=[16]
-NUMBER_CORES_PER_NODE=1
+NUMBER_CORES_PER_NODE=8
 RESULT_DIR="results"
 RESULT_FILE_PREFIX="results/results-"
-LRMS_URL="fork://localhost"
+LRMS_URL="pbspro://localhost"
 
 
 def has_finished(state):
@@ -94,7 +102,7 @@ def load_test(coordination_url, number_jobs, number_nodes, number_cores_per_node
     subjob_submission_time = time.time()-starttime
     # busy wait for completion
     while 1:        
-        pilot_state = str(bj.get_state())
+        pilot_state = str(bj.get_state_detail())
         if pilot_state=="Running" and queueing_time==None:
             queueing_time=time.time()-starttime
             print "*** Pilot State: " + pilot_state + " queue time: " + str(queueing_time)
@@ -137,6 +145,7 @@ def load_test(coordination_url, number_jobs, number_nodes, number_cores_per_node
     bj.cancel()
     # hack: delete manually pbs jobs of user
     os.system("qstat -u `whoami` | grep -o ^[0-9]* |xargs qdel")
+    os.system("saga-advert remove_directory advert://advert.cct.lsu.edu:8080/bigjob")
     return result
           
     
@@ -151,9 +160,14 @@ if __name__ == "__main__":
     result_filename = RESULT_FILE_PREFIX + d.strftime("%Y%m%d-%H%M%S") + ".csv"
     f = open(result_filename, "w")
     f.write("#Nodes,#cores/node,#jobs,Runtime,Queuing Time,Coordination URL,LRMS URL,SubJob Submission Time\n")
-    for i in range(0, len(NUMBER_JOBS)):
-        result = load_test(COORDINATION_URL, NUMBER_JOBS[i], NUMBER_NODES[i], NUMBER_CORES_PER_NODE)
-        f.write(result)
-        f.write("\n")
-        f.flush()
+    for k in range(0, NUMBER_RUNS):
+        print ("\n**************\n RUN %d \n*****************\n"%k)
+        for j in range(0, len(COORDINATION_URL)):
+            c = COORDINATION_URL[j]
+            print ("\n**************\n COORDINATION: %s \n*****************\n"%c)
+            for i in range(0, len(NUMBER_JOBS)):
+                result = load_test(c, NUMBER_JOBS[i], NUMBER_NODES[i], NUMBER_CORES_PER_NODE)
+                f.write(result)
+                f.write("\n")
+                f.flush()
     f.close()
