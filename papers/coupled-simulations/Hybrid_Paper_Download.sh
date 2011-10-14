137f4f4ad78fb8d2468daa3d2088fc907e9839a5
sko <sko@defb5e50-622e-49ec-a68e-d72c7db87b45>
2011-10-14 17:16:54 +0000

    Templates for new paper / downloading the paper with Bibs as the subdirectory
    git-svn-id: file://localhost/tmp/svn2git/svn@5506 defb5e50-622e-49ec-a68e-d72c7db87b45

---
+++ papers/coupled-simulations/Hybrid_Paper_Download.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+### Name of the new paper directory
+Paper="eScience11"
+### Name of repository home
+Repository="https://svn.cct.lsu.edu/repos/saga-projects/papers/coupled-simulations"
+### Name of reference Bibs 1: saga files
+SAGA_Bib="https://svn.cct.lsu.edu/repos/saga-projects/papers/Compiled_bibentries"
+### Name of reference Bibs 2: our Bibs
+Local_Bib="https://svn.cct.lsu.edu/repos/saga-projects/papers/coupled-simulations/Bibs"
+
+### Checkout
+svn co $Repository/$Paper
+
+cd $Paper
+
+### Getting Hybrid Bib file
+svn co $Local_Bib
+
+cd Bibs
+
+### Reference to SAGA Bibs (get informed on ''export'')
+svn export $SAGA_Bib/saga.bib 
+svn export $SAGA_Bib/saga-related.bib
+
+cd ..
