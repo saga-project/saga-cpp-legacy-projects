c2bb14fcf4ce179b683d66b33da4fc06ea8e37b8
amerzky <amerzky@defb5e50-622e-49ec-a68e-d72c7db87b45>
2011-12-01 13:49:43 +0000

    snap
    git-svn-id: file://localhost/tmp/svn2git/svn@6001 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/master_worker/example/hello_world/mw_master.cpp
+++ applications/master_worker/example/hello_world/mw_master.cpp
@@ -12,30 +12,24 @@ int main ()
     saga_pm::master_worker::master m;
 
     m.initialize ("test");
-    std::cout << " ====== 1 " << std::endl;
-
 
     saga_pm::master_worker::worker_description wd;
 
     std::vector <std::string> args;
     args.push_back ("mw_worker");
-    std::cout << " ====== 2 " << std::endl;
 
     wd.rm = "fork://localhost/";
     wd.jd.set_attribute        (saga::job::attributes::description_executable, "saga-run.sh");
     wd.jd.set_vector_attribute (saga::job::attributes::description_arguments,  args);
 
     id_t id = m.worker_start (wd);
-    m.worker_dump (id);
-    std::cout << " ====== 3 " << std::endl;
+ // m.worker_dump (id);
 
     m.worker_run  (id, "test");
-    m.worker_dump (id);
-    std::cout << " ====== 4 " << std::endl;
+ // m.worker_dump (id);
 
     m.worker_wait (id);
-    m.worker_dump (id);
-    std::cout << " ====== 5 " << std::endl;
+ // m.worker_dump (id);
 
     saga_pm::master_worker::argvec_t res = m.worker_get_results (id);
     std::cout << "results for test()" << std::endl;
@@ -43,12 +37,13 @@ int main ()
     {
       std::cout << "  " << i << " : " << res[i] << std::endl;
     }
-    std::cout << " ====== 6 " << std::endl;
 
 
     m.worker_run  (id, "quit");
-    m.worker_dump (id);
-    std::cout << " ====== 7 " << std::endl;
+ // m.worker_dump (id);
+
+    m.worker_wait (id);
+ // m.worker_dump (id);
 
     LOG << "MASTER DONE" << std::endl;
   }
