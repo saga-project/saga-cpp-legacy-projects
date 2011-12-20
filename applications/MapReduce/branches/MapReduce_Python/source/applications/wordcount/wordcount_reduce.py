74d7d946b8951e0b369692cd9f5e1edff3e6a937
pmanth2 <pmanth2@defb5e50-622e-49ec-a68e-d72c7db87b45>
2011-12-20 12:26:45 +0000

    updated reduce, chunk files
    git-svn-id: file://localhost/tmp/svn2git/svn@6144 defb5e50-622e-49ec-a68e-d72c7db87b45

--- applications/MapReduce/branches/MapReduce_Python/source/applications/wordcount/wordcount_reduce.py
+++ applications/MapReduce/branches/MapReduce_Python/source/applications/wordcount/wordcount_reduce.py
@@ -5,14 +5,16 @@ import os
 
 # input comes from STDIN (standard input)
 part_files=sys.argv[1]
+output_dir=sys.argv[2]
 print part_files
 part_list=part_files.split(":")
 part_list=part_list[1:]
 key_count={}
 k=str(part_list[0].split("-")[-1:][0])
-reduce_path=(os.path.split(part_list[0]))[0]
-reduce_file=reduce_path+"/reduce-"+str(k)
+reduce_file=output_dir+"/reduce-"+str(k)
 reduce_write=open(reduce_file,'w')
+
+print os.getcwd()
 
 for i in part_list:
     print " file name " + i
@@ -20,7 +22,7 @@ for i in part_list:
     for line in part_file:
         words=line.split("DELIM,")
         words[1]=words[1].rstrip()
-        print str(words)+ "\n"
+        #print str(words)+ "\n"
         if words[0] not in key_count:
            key_count[words[0]] = 0
            key_count[words[0]] = key_count[words[0]]+int(words[1])
